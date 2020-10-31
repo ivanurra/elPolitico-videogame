53d5148b3fcfef80279b7bb757ba9a2c6b8bbe70
Ivan Urra <ivan.urra.wd@gmail.com>
2020-10-31 21:43:54 +0100

    Canvas

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,23 @@
-// CHANGE WINDOWS
-function changeWindows(id, id2){
-    if(document.getElementById){
-        let element = document.getElementById(id)
-        element.style.display = (element.style.display == 'none') ? 'flex' : 'none';
+const airport = new Image()
+airport.src = './media/background.png'
+let airportX = 0
+let airportY = 0
+// function draw(x,y){
+//     const canvas = document.getElementById('canvas')
+//     const ctx = canvas.getContext("2d")
+//     ctx.clearRect(0, 0, 300, 300)
+//     ctx.drawImage(airport, airportX, airportY, 50, 50);
+// }
+// draw()
 
-        let element2 = document.getElementById(id2)
-        element2.style.display = (element2.style.display == 'flex') ? 'none' : 'flex';
-    }
+window.onload = function(){
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext("2d")
+    ctx.drawImage(airport, 0, 0, 1200, 533);
 }
+
+// REALIZAR CON CLASES. PRUEBA A CAMBIAR EL NOMBRE DE LA CLASE POR IMG EN LUGAR DE AIRPORT. LUEGO
+// CREAS LA VARIABLE AIRPORT POR OTRO LADO.
+
+
+
